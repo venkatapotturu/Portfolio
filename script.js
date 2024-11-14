d051e526e89501b21bc202907dc61756405a2a25
kruthi potturi <vysho@MacBook-Air.local>
2024-11-13 23:45:05 -0700

    Initial commit for the website

---
+++ script.js
@@ -0,0 +1,36 @@
+new Swiper('.card-wrapper', {
+    
+    loop: true,
+    spaceBetween:30,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    //Responsive Breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+    }
+});
+
+
+function toggleMobileMenu(){
+    document.getElementById("menu").classList.toggle
+    ("active");
+}
